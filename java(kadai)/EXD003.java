435501e9d39280932233921b3bef31ef1bd394b8
momizi-rr <natsuyume2@gmail.com>
2017-03-22 00:56:01 +0900

    0321

--- java(kadai)/EXD003.java
+++ java(kadai)/EXD003.java
@@ -2,14 +2,14 @@ public class EXD003 {
    public static void main(String[] args)    {
       try{
             for(int i = 10; i >= 0; i--){
-            Thread.sleep(1000);
+            Thread.sleep(1000); //1000ミリ秒停止
             if(i>0){
                System.out.println("あと"+i+"秒");
             }else{
                System.out.println("明けまして、おめでとうノシ");
             }
          }
-         for(int i = 1; i <= 10; i++){
+         for(int i = 1; i <= 10; i++){   //経過表示
          Thread.sleep(1000);
          System.out.println(i+" 秒経過");
          }
